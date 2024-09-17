dcf7a66eba0cb9ee23c2d596822b3bfd0862a329
ReatoX <reatox@outlook.com>
2024-09-17 21:27:27 +0100

    Improvements: - Add Next Top Loader

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { Navbar } from "@/components/navbar";
+import NextTopLoader from "nextjs-toploader";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -29,6 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased h-full flex flex-col`}
       >
+        <NextTopLoader color="#ffffff" />
         <Navbar />
         <main className="flex-grow flex items-center justify-center">{children}</main>
       </body>
